b2e9e6cfcaeb20842870eca01d24aa88c361cbe3
Sanyam Goel <entitylabs@gmail.com>
2021-07-22 18:48:16 +0200

    Added initial code

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GenericModule } from './generic/generic.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GenericModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/generic/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product.model';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent implements OnInit {
+
+
+  product:Product={quantity:0, name:"First product"};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
